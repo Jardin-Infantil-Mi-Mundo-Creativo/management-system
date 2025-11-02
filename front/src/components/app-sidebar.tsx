5819bb86c55d2f936fa7b5047d7928856a9f7313
sfrancop <s.francop@uniandes.edu.co>
2025-11-02 08:26:08 -0500

    feat: :heavy_plus_sign: setup shadcn and use sidebar component

---
+++ front/src/components/app-sidebar.tsx
@@ -0,0 +1,46 @@
+import { UserPlus } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const items = [
+  {
+    title: "Matricular estudiante",
+    url: "/matricular-estudiante",
+    icon: UserPlus,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar variant='floating'>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Jardín Infantil Mi Mundo Creativo</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
