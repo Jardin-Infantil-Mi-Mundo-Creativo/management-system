c69816f6824939183ec7aee26ebcb700ff81778a
sfrancop <s.francop@uniandes.edu.co>
2025-11-16 22:24:31 -0500

    refactor: 🏗 configure husky in the root

--- front/src/components/enrollment/enrollment-form-section-personal-student-info.tsx
+++ front/src/components/enrollment/enrollment-form-section-personal-student-info.tsx
@@ -1,16 +1,29 @@
-import { EnrollmentFormSectionHeader } from '@/components/enrollment/enrollment'
-import { InputGroup } from '@/components/ui/input-group'
-import { calculateAgeMonths, calculateAgeYears } from '@/utils/enrollment/calculate-age';
-import { ControlledDatePicker } from '@/components/enrollment/controlled-date-picker'
-import type { Control, FieldError, FieldErrorsImpl, Merge, UseFormRegister, UseFormSetValue } from 'react-hook-form';
+import { EnrollmentFormSectionHeader } from '@/components/enrollment/enrollment';
+import { InputGroup } from '@/components/ui/input-group';
+import {
+  calculateAgeMonths,
+  calculateAgeYears,
+} from '@/utils/enrollment/calculate-age';
+import { ControlledDatePicker } from '@/components/enrollment/controlled-date-picker';
+import type {
+  Control,
+  FieldError,
+  FieldErrorsImpl,
+  Merge,
+  UseFormRegister,
+  UseFormSetValue,
+} from 'react-hook-form';
 import type { EnrollmentFormSchema } from '@/types/enrollment';
 
 interface EnrollmentFormSectionPersonalStudentInfoProps {
   register: UseFormRegister<EnrollmentFormSchema>;
   control: Control<EnrollmentFormSchema>;
-  personalStudentInfoErrors?: Merge<FieldError, FieldErrorsImpl<EnrollmentFormSchema['personalStudentInfo']>>;
+  personalStudentInfoErrors?: Merge<
+    FieldError,
+    FieldErrorsImpl<EnrollmentFormSchema['personalStudentInfo']>
+  >;
   setValue: UseFormSetValue<EnrollmentFormSchema>;
-};
+}
 
 function EnrollmentFormSectionPersonalStudentInfo({
   register,
@@ -25,45 +38,45 @@ function EnrollmentFormSectionPersonalStudentInfo({
       </EnrollmentFormSectionHeader>
 
       <InputGroup
-        className='w-full'
+        className="w-full"
         label="Nombre completo:"
         inputId="personalStudentInfo.fullName"
-        register={register("personalStudentInfo.fullName")}
+        register={register('personalStudentInfo.fullName')}
         errorMessage={personalStudentInfoErrors?.fullName?.message}
       />
 
-      <div className='flex gap-4 flex-wrap'>
+      <div className="flex gap-4 flex-wrap">
         <ControlledDatePicker
           control={control}
-          inputId='personalStudentInfo.birthDate'
-          labelText='Fecha de nacimiento:'
+          inputId="personalStudentInfo.birthDate"
+          labelText="Fecha de nacimiento:"
           errorMessage={personalStudentInfoErrors?.birthDate?.message}
           onValueChange={(value) => {
-            const calculatedAgeYears = calculateAgeYears(value)
-            setValue("personalStudentInfo.ageYears", calculatedAgeYears)
-            const calculatedAgeMonths = calculateAgeMonths(value)
-            setValue("personalStudentInfo.ageMonths", calculatedAgeMonths)
+            const calculatedAgeYears = calculateAgeYears(value);
+            setValue('personalStudentInfo.ageYears', calculatedAgeYears);
+            const calculatedAgeMonths = calculateAgeMonths(value);
+            setValue('personalStudentInfo.ageMonths', calculatedAgeMonths);
           }}
         />
 
         <InputGroup
-          className='w-16'
+          className="w-16"
           label="Años:"
           inputId="personalStudentInfo.ageYears"
-          register={register("personalStudentInfo.ageYears")}
+          register={register('personalStudentInfo.ageYears')}
           disabled={true}
         />
 
         <InputGroup
-          className='w-16'
+          className="w-16"
           label="Meses:"
           inputId="personalStudentInfo.ageMonths"
-          register={register("personalStudentInfo.ageMonths")}
+          register={register('personalStudentInfo.ageMonths')}
           disabled={true}
         />
 
         <InputGroup
-          className='min-w-48'
+          className="min-w-48"
           label="Ciudad de nacimiento:"
           inputId="personalStudentInfo.birthCity"
           register={register('personalStudentInfo.birthCity')}
@@ -71,15 +84,17 @@ function EnrollmentFormSectionPersonalStudentInfo({
         />
 
         <InputGroup
-          className='min-w-48'
+          className="min-w-48"
           label="N° Registro Civil:"
           inputId="personalStudentInfo.civilRegistrationNumber"
           register={register('personalStudentInfo.civilRegistrationNumber')}
-          errorMessage={personalStudentInfoErrors?.civilRegistrationNumber?.message}
+          errorMessage={
+            personalStudentInfoErrors?.civilRegistrationNumber?.message
+          }
         />
       </div>
     </>
-  )
+  );
 }
 
 export { EnrollmentFormSectionPersonalStudentInfo };
