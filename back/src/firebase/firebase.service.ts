2d626de7addc6267ba44eef882cc3eeef0bf673d
sfrancop <s.francop@uniandes.edu.co>
2025-11-17 22:38:23 -0500

    ci: :construction_worker: add replace again

--- back/src/firebase/firebase.service.ts
+++ back/src/firebase/firebase.service.ts
@@ -15,7 +15,10 @@ export class FirebaseService {
         type: process.env.BACK_FIREBASE_TYPE,
         project_id: process.env.BACK_FIREBASE_PROJECT_ID,
         private_key_id: process.env.BACK_FIREBASE_PRIVATE_KEY_ID,
-        private_key: process.env.BACK_FIREBASE_PRIVATE_KEY,
+        private_key: process.env.BACK_FIREBASE_PRIVATE_KEY?.replace(
+          /\\n/g,
+          '\n',
+        ),
         client_email: process.env.BACK_FIREBASE_CLIENT_EMAIL,
         client_id: process.env.BACK_FIREBASE_CLIENT_ID,
         auth_uri: process.env.BACK_FIREBASE_AUTH_URI,
